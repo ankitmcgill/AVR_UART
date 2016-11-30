c8c011b8ae2c54770ae1b2d1f8913e40aaee38af
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-30 18:21:55 +0530

    added basic functions skeleton

--- AVR_UART.c
+++ AVR_UART.c
@@ -8,3 +8,45 @@
 //////////////////////////////////////////////
 
 #include "AVR_UART.h"
+
+int8_t AVR_UART_init(void)
+{
+	//INTIALIZE UART PERIPHERAL
+	//WITH THE SPECIFIED PARAMETERS
+}
+
+int8_t AVR_UART_enable(void)
+{
+	//ENABLE (CLOCK TO) UART PERIPHERAL
+}
+
+int8_t AVR_UART_disable(void)
+{
+	//DISBALE (CLOCK TO) UART PERIPHERAL
+
+}
+
+int8_t AVR_UART_data_send(uint8_t data)
+{
+	//SEND SINGLE BYTE OF DATA THROUGH THE
+	//UART PERIPHERAL
+
+}
+
+int8_t AVR_UART_data_send_block(uint8_t* data_block, uint8_t size)
+{
+	//SEND MULTIPLE BYTES OF DATA THROUGH UART PERIPHERAL
+
+}
+
+int8_t AVR_UART_data_receive(uint8_t* data)
+{
+	//RECEIVE SINGLE BYTE OF DATA THROUGH UART PERIPHERAL
+
+}
+
+int8_t AVR_UART_data_receive_block(uint8_t* data_block, uint8_t size)
+{
+	//RECEIVE MULTIPLE BYTES OF DATA THROUGH UART PERIPHERAL
+
+}
--- AVR_UART.h
+++ AVR_UART.h
@@ -10,5 +10,19 @@
 #ifndef _AVR_UART_H_
 #define _AVR_UART_H_
 
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int8_t AVR_UART_init(void);
+
+int8_t AVR_UART_enable(void);
+int8_t AVR_UART_disable(void);
+
+int8_t AVR_UART_data_send(uint8_t data);
+int8_t AVR_UART_data_send_block(uint8_t* data_block, uint8_t size);
+
+int8_t AVR_UART_data_receive(uint8_t* data);
+int8_t AVR_UART_data_receive_block(uint8_t* data_block, uint8_t size);
 
 #endif
