16e8d7dc42d015f25ee120e5b0aef1c5b2668e01
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-30 12:26:28 +0530

    Initial file commit

---
+++ AVR_UART.h
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////
+// AVR UART LIBRARY
+//
+// NOVEMBER 30, 2016
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+//////////////////////////////////////////////
+
+#ifndef _AVR_UART_H_
+#define _AVR_UART_H_
+
+
+#endif
