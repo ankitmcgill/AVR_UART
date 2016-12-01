09c92c2920ff5ed2581607304549124ba40b0796
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-01 13:56:11 +0530

    added init/enable/disable code and implemented single and block tx functions

--- AVR_UART.c
+++ AVR_UART.c
@@ -1,6 +1,8 @@
 //////////////////////////////////////////////
 // AVR UART LIBRARY
 //
+// ONLY IMPLEMENTS THE ASYNCHRONOUS MODE
+//
 // NOVEMBER 30, 2016
 //
 // ANKIT BHATNAGAR
@@ -9,43 +11,103 @@
 
 #include "AVR_UART.h"
 
-int8_t AVR_UART_init(void)
+int8_t AVR_UART_init(uint16_t baud, uint8_t data_size, uint8_t parity, uint8_t stop_bits)
 {
 	//INTIALIZE UART PERIPHERAL
 	//WITH THE SPECIFIED PARAMETERS
+	
+	//SET BAUDRATE
+	uint16_t ubbr_val = (AVR_UART_FOSC / (baud * 16UL)) - 1;
+	UBRR0H = (uint8_t)(ubbr_val>>8); //set UBRR register
+	UBRR0L = (uint8_t)ubbr_val;
+	
+	//SET REMAINING PARAMETERS
+	UCSR0C |= parity;
+	UCSR0C |= stop_bits;
+
+	switch(data_size)
+	{
+		case AVR_UART_DATASIZE_5:	//NOTHING REQUIRED
+									break;
+		
+		case AVR_UART_DATASIZE_6:	
+									UCSR0C |= (1<<1);
+									break;
+		
+		case AVR_UART_DATASIZE_7:	
+									UCSR0C |= (1<<2);
+									break;
+
+		case AVR_UART_DATASIZE_8:	
+									UCSR0C |= ((1<<2) | (1<<1));
+									break;
+
+		case AVR_UART_DATASIZE_9:	
+									UCSR0C |= ((1<<2) | (1<<1));
+									UCSR0B |= (1<<2);
+									break;
+		default:
+									return AVR_UART_ERROR;
+	}
+
+	return AVR_UART_OK;
 }
 
 int8_t AVR_UART_enable(void)
 {
 	//ENABLE (CLOCK TO) UART PERIPHERAL
+	//ENABLE BOTH TX AND RX
+
+	UCSR0B |= ((1<<4) | (1<<3));
+	return AVR_UART_OK;
 }
 
 int8_t AVR_UART_disable(void)
 {
 	//DISBALE (CLOCK TO) UART PERIPHERAL
+	//DISABLE BOTH TX AND RX
 
+	UCSR0B &= ~((1<<4) | (1<<3));
+	return AVR_UART_OK;
 }
 
-int8_t AVR_UART_data_send(uint8_t data)
+int8_t AVR_UART_data_tx(uint8_t data)
 {
 	//SEND SINGLE BYTE OF DATA THROUGH THE
 	//UART PERIPHERAL
 
+	//ENSURE UART TX DATA REGISTER EMPTY
+	while((UCSR0A & (1<<UDRE0)) == 0);
+	UDR0 = data;
+
+	return AVR_UART_OK;
 }
 
-int8_t AVR_UART_data_send_block(uint8_t* data_block, uint8_t size)
+int8_t AVR_UART_data_tx_block(uint8_t* data_block, uint8_t size)
 {
 	//SEND MULTIPLE BYTES OF DATA THROUGH UART PERIPHERAL
 
+	for(uint8_t i=size; size>0; size--)
+	{
+		while((UCSR0A & (1<<UDRE0)) == 0);
+		UDR0 = *data_block++;
+	}
+
+	return AVR_UART_OK;
 }
 
-int8_t AVR_UART_data_receive(uint8_t* data)
+int8_t AVR_UART_data_rx(uint8_t* data)
 {
 	//RECEIVE SINGLE BYTE OF DATA THROUGH UART PERIPHERAL
 
+	//WAIT FOR DATA TO BE AVAILABLE
+	while((UCSR0A & (1<<RXC0)) == 0);
+	*data = UDR0;
+
+	return AVR_UART_OK;
 }
 
-int8_t AVR_UART_data_receive_block(uint8_t* data_block, uint8_t size)
+int8_t AVR_UART_data_rx_block(uint8_t* data_block, uint8_t size)
 {
 	//RECEIVE MULTIPLE BYTES OF DATA THROUGH UART PERIPHERAL
 
--- AVR_UART.h
+++ AVR_UART.h
@@ -1,6 +1,8 @@
 //////////////////////////////////////////////
 // AVR UART LIBRARY
 //
+// ONLY IMPLEMENTS THE ASYNCHRONOUS MODE
+//
 // NOVEMBER 30, 2016
 //
 // ANKIT BHATNAGAR
@@ -14,15 +16,37 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int8_t AVR_UART_init(void);
+#define AVR_UART_FOSC		16000000
+
+#define AVR_UART_B4800		4800
+#define AVR_UART_B9600		9600
+#define AVR_UART_B115200	115200
+
+#define AVR_UART_OK			0
+#define AVR_UART_ERROR		-1
+
+#define AVR_UART_DATASIZE_5	5
+#define AVR_UART_DATASIZE_6	6
+#define AVR_UART_DATASIZE_7	7
+#define AVR_UART_DATASIZE_8	8
+#define AVR_UART_DATASIZE_9	9
+
+#define AVR_UART_PARITY_NO		(0)
+#define AVR_UART_PARITY_EVEN	(1<<5)
+#define AVR_UART_PARITY_ODD		((1<<5)|(1<<4))
+
+#define AVR_UART_STOPBITS_1	(0)
+#define AVR_UART_STOPBITS_2	(1<<3)
+
+int8_t AVR_UART_init(uint16_t baud, uint8_t data_size, uint8_t parity, uint8_t stop_bits);
 
 int8_t AVR_UART_enable(void);
 int8_t AVR_UART_disable(void);
 
-int8_t AVR_UART_data_send(uint8_t data);
-int8_t AVR_UART_data_send_block(uint8_t* data_block, uint8_t size);
+int8_t AVR_UART_data_tx(uint8_t data);
+int8_t AVR_UART_data_tx_block(uint8_t* data_block, uint8_t size);
 
-int8_t AVR_UART_data_receive(uint8_t* data);
-int8_t AVR_UART_data_receive_block(uint8_t* data_block, uint8_t size);
+int8_t AVR_UART_data_rx(uint8_t* data);
+int8_t AVR_UART_data_rx_block(uint8_t* data_block, uint8_t size);
 
 #endif
