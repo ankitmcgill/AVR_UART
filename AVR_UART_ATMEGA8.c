7f7a7f2effdc5290bbab16870b1305ba593ed7ce
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-26 14:04:32 +0530

    added skeleton files for atmega8 library

---
+++ AVR_UART_ATMEGA8.c
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////
+// AVR UART LIBRARY
+// FOR ATMEGA8
+//
+// ONLY IMPLEMENTS THE ASYNCHRONOUS MODE
+//
+// DECEMBER 26, 2016
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+//////////////////////////////////////////////
+
+#include "AVR_UART_ATMEGA8.h"
---
+++ AVR_UART_ATMEGA8.h
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////
+// AVR UART LIBRARY
+// FOR ATMEGA8
+//
+// ONLY IMPLEMENTS THE ASYNCHRONOUS MODE
+//
+// DECEMBER 26, 2016
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+//////////////////////////////////////////////
+
+#ifndef _AVR_UART_ATMEGA8_H_
+#define _AVR_UART_ATMEGA8_H_
+
+
+#endif
