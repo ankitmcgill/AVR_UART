f2884dc0f41187f7b19f09d52d8aa642f57c6c74
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-27 16:41:24 +0530

    added uart code to the library for atmega8

--- AVR_UART_ATMEGA8.c
+++ AVR_UART_ATMEGA8.c
@@ -11,3 +11,143 @@
 //////////////////////////////////////////////
 
 #include "AVR_UART_ATMEGA8.h"
+
+int8_t AVR_UART_Init(uint16_t baud, uint8_t data_size, uint8_t parity, uint8_t stop_bits, uint8_t u2x)
+{
+	//INTIALIZE UART PERIPHERAL
+	//WITH THE SPECIFIED PARAMETERS
+	
+	//SET BAUDRATE
+	uint16_t ubbr_val = (F_CPU / (baud * 16UL)) - 1;
+	UBRRH = (uint8_t)(ubbr_val >> 8); //set UBRR register
+	UBRRL = (uint8_t)ubbr_val;
+	
+	//SET REMAINING PARAMETERS
+	UCSRC |= parity;
+	UCSRC |= stop_bits;
+
+	//SET U2X IF ENABLED
+	UCSRA |= u2x;
+
+	switch(data_size)
+	{
+		case AVR_UART_DATASIZE_5:	//NOTHING REQUIRED
+									break;
+		
+		case AVR_UART_DATASIZE_6:	
+									UCSRC |= (1 << 1);
+									break;
+		
+		case AVR_UART_DATASIZE_7:	
+									UCSRC |= (1 << 2);
+									break;
+
+		case AVR_UART_DATASIZE_8:	
+									UCSRC |= ((1 << 2) | (1 << 1));
+									break;
+
+		case AVR_UART_DATASIZE_9:	
+									UCSRC |= ((1 << 2) | (1 << 1));
+									UCSRB |= (1 << 2);
+									break;
+		default:
+									return AVR_UART_ERROR;
+	}
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Enable(void)
+{
+	//ENABLE (CLOCK TO) UART PERIPHERAL
+	//ENABLE BOTH TX AND RX
+
+	UCSRB |= ((1 << 4) | (1 << 3));
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Disable(void)
+{
+	//DISBALE (CLOCK TO) UART PERIPHERAL
+	//DISABLE BOTH TX AND RX
+
+	UCSRB &= ~((1 << 4) | (1 << 3));
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Data_Tx(uint8_t data)
+{
+	//SEND SINGLE BYTE OF DATA THROUGH THE
+	//UART PERIPHERAL
+
+	//ENSURE UART TX DATA REGISTER EMPTY
+	while((UCSRA & (1 << UDRE)) == 0);
+	UDR = data;
+
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Data_Tx_Block(uint8_t* data_block, uint8_t size)
+{
+	//SEND MULTIPLE BYTES OF DATA THROUGH UART PERIPHERAL
+
+	while(size != 0)
+	{
+		AVR_UART_Data_Tx(*data_block++);
+		size--;
+	}
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Data_Rx_Blocking(uint8_t* data)
+{
+	//RECEIVE SINGLE BYTE OF DATA THROUGH UART PERIPHERAL
+	//BLOCKS TILL DATA AVAILABLE
+
+	//WAIT FOR DATA TO BE AVAILABLE
+	while((UCSRA & (1 << RXC)) == 0);
+	*data = UDR;
+
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Data_Rx_Non_Blocking(uint8_t* data)
+{
+	//RECEIVE SINGLE BYTE OF DATA THROUGH UART PERIPHERAL
+	//NON BLOCKING. RETURNS ERROR IF NO DATA AVAILABLE
+
+	//CHECK IF DATA AVAILABLE
+	if((UCSRA & (1 << RXC)) != 0)
+	{
+		*data = UDR;
+	}
+	else
+	{
+		return AVR_UART_ERROR;
+	}
+	return AVR_UART_OK;
+}
+
+int8_t AVR_UART_Data_Rx_Block(uint8_t* data_block, uint8_t* size)
+{
+	//RECEIVE MULTIPLE BYTES OF DATA THROUGH UART PERIPHERAL
+	//GET DATA TILL UART SIGNALS RX DATA PRESENT
+	//THEN COMPARE THE NUMBER OF BYTES RECEIVED WITH NO OF BYTES
+	//REQUESTED AND DEPENDING ON THAT, RETURN OK OR (ERROR + MODIFY
+	//SIZE PARAMETER POINTER TO THE NUMBER OF BYTES ACTUALLY READ
+	
+	uint8_t counter = 0;
+	
+	while(counter != (*size))
+	{
+		AVR_UART_Data_Rx_Blocking(data_block);
+
+		//AVR_UART_data_tx(*data_block);
+
+		counter++;
+		data_block++;
+	}
+
+	//AVR_UART_data_tx('\n');
+
+	return AVR_UART_OK;
+}
--- AVR_UART_ATMEGA8.h
+++ AVR_UART_ATMEGA8.h
@@ -13,5 +13,45 @@
 #ifndef _AVR_UART_ATMEGA8_H_
 #define _AVR_UART_ATMEGA8_H_
 
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define AVR_UART_B2400		2400
+#define AVR_UART_B4800		4800
+#define AVR_UART_B4800		4800
+#define AVR_UART_B9600		9600
+#define AVR_UART_B115200	115200
+
+#define AVR_UART_OK			0
+#define AVR_UART_ERROR		-1
+
+#define AVR_UART_DATASIZE_5	5
+#define AVR_UART_DATASIZE_6	6
+#define AVR_UART_DATASIZE_7	7
+#define AVR_UART_DATASIZE_8	8
+#define AVR_UART_DATASIZE_9	9
+
+#define AVR_UART_PARITY_NO		(0)
+#define AVR_UART_PARITY_EVEN	(1<<5)
+#define AVR_UART_PARITY_ODD		((1<<5)|(1<<4))
+
+#define AVR_UART_STOPBITS_1	(0)
+#define AVR_UART_STOPBITS_2	(1<<3)
+
+#define AVR_UART_U2X_OFF	(0)
+#define AVR_UART_U2X_ON		(1<<1)
+
+int8_t AVR_UART_Init(uint16_t baud, uint8_t data_size, uint8_t parity, uint8_t stop_bits, uint8_t u2x);
+
+int8_t AVR_UART_Enable(void);
+int8_t AVR_UART_Disable(void);
+
+int8_t AVR_UART_Data_Tx(uint8_t data);
+int8_t AVR_UART_Data_Tx_Block(uint8_t* data_block, uint8_t size);
+
+int8_t AVR_UART_Data_Rx_Blocking(uint8_t* data);
+int8_t AVR_UART_Data_Rx_Non_Blocking(uint8_t* data);
+int8_t AVR_UART_Data_Rx_Block(uint8_t* data_block, uint8_t* size);
 
 #endif
